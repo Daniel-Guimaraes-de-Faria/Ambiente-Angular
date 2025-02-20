2d4d5039169f95800062f23e8b22f78a7e4c5511
Daniel Guimarães de Faria <164964227+Daniel-Guimaraes-de-Faria@users.noreply.github.com>
2025-02-20 00:15:01 +0000

    Alterando arquivos APP, fazendo os exercicios.

--- projeto-angular/src/app/app-routing.module.ts
+++ projeto-angular/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'livros', loadChildren: () => import('./livros/livros.module').then(m=> m.LivrosModule)   }
+
 ];
 
 @NgModule({
--- projeto-angular/src/app/app.module.ts
+++ projeto-angular/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; // Importa o FormsModule
 
 @NgModule({
   declarations: [
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule // Adiciona o FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
