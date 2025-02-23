7a4ed43f533cfaf6db498563008dbf05aac77249
Daniel Guimarães de Faria <164964227+Daniel-Guimaraes-de-Faria@users.noreply.github.com>
2025-02-23 21:09:23 +0000

    Ajustes

--- projeto-angular/src/app/app.component.ts
+++ projeto-angular/src/app/app.component.ts
@@ -7,27 +7,25 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  title: string = 'Gerenciador de Tarefas'; // Título do aplicativo
-  totalTasks: number = 0; // Total de tarefas
-  mostrarTarefas: boolean = true; // Controla a exibição da lista de tarefas
-  nomeTarefa: string = ''; // Captura o nome da nova tarefa
-  tarefas: { nome: string, concluida: boolean }[] = []; // Lista de tarefas
+  title: string = 'Gerenciador de Tarefas';
+  totalTasks: number = 0;
+  mostrarTarefas: boolean = true;
+  nomeTarefa: string = '';
+  tarefas: { nome: string, concluida: boolean }[] = [];
 
-  // Função para adicionar nova tarefa
   adicionarTarefa(): void {
     if (this.nomeTarefa.trim()) {
       this.tarefas.push({ nome: this.nomeTarefa, concluida: false });
-      this.nomeTarefa = ''; // Limpa o campo após adicionar
+      this.nomeTarefa = '';
       this.totalTasks = this.tarefas.length;
     }
   }
 
-  // Função para marcar tarefa como concluída
   marcarConcluida(tarefa: any): void {
     tarefa.concluida = !tarefa.concluida;
   }
 
-  // Função para alternar a visibilidade da lista
+
   alternarVisibilidade(): void {
     this.mostrarTarefas = !this.mostrarTarefas;
   }
--- projeto-angular/src/app/app.module.ts
+++ projeto-angular/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'; // Importa o FormsModule
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms'; // Importa o FormsModule
   ],
   imports: [
     BrowserModule,
-    FormsModule // Adiciona o FormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
